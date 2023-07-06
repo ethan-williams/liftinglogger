1e98ddd3bf14518a09fccfb08cc2d177191becf9
Avery camou <ecwilli8@asu.edu>
2023-07-06 01:10:10 -0600

    :construction: Improve code readability of fruit.js

--- fruit.js
+++ fruit.js
@@ -6,29 +6,20 @@ const jsonFruits = require('./data/fruits.json');
  * Takes a force in pounds and returns a comment about which fruit you can squash
  */
 function comment(_force) {
-	let comment;
-	let strongestFruitIndex = jsonFruits.fruits.length - 1;
+	var comment;
 
 	if ( _force < jsonFruits.fruits[0].force ) {
-		// User is a precious baby with no fruit crushing powers
-		comment = "I don't think you can crush any fruit with your thighs. Sorry!";
-	} else if (_force > jsonFruits.fruits[strongestFruitIndex].force) {
-		// User is a beast and can crush the strongest fruit in our database
-		comment = jsonFruits.fruits[strongestFruitIndex].comment;
+		comment = "I don't know of a fruit you can crush. Sorry!";
 	} else {
-		for(let i = 0; i < jsonFruits.fruits.length; i++) {
-			if (jsonFruits.fruits[i].force == _force) {
-				// Current fruit's force matched the user's force so set its comment
+		var max = Number.MIN_VALUE;
+		for (var i = 0; i < jsonFruits.fruits.length; i++) {
+			if (jsonFruits.fruits[i].force <= _force && jsonFruits.fruits[i].force > max) {
+				max = jsonFruits.fruits[i].force;
 				comment = jsonFruits.fruits[i].comment;
-				break;
-			} else if (jsonFruits.fruits[i].force > _force) {
-				// Current fruit's force exceeds the user's force so set the previous fruit's
-				// comment
-				comment = jsonFruits.fruits[i - 1].comment;
-				break;
 			}
 		}
 	}
+
 	return comment;
 }
 
